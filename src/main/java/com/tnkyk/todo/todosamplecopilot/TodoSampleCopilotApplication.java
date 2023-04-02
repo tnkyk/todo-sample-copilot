eaf246b9715e09ba99d7c051c91ee6f88b00730f
tnkyk <yuukiyuuki327@gmail.com>
2023-04-02 10:16:19 +0900

    first commit

---
+++ src/main/java/com/tnkyk/todo/todosamplecopilot/TodoSampleCopilotApplication.java
@@ -0,0 +1,13 @@
+package com.tnkyk.todo.todosamplecopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSampleCopilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoSampleCopilotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tnkyk/todo/todosamplecopilot/TodoSampleCopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnkyk.todo.todosamplecopilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSampleCopilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
